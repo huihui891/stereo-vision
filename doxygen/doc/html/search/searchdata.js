53e35a35051f5dc5f54ef5f313efbde8d89d1ae3
Ugo Pattacini <ugo.pattacini@iit.it>
2014-09-03 17:52:56 +0200

    provided doxygen documentation

---
+++ doxygen/doc/html/search/searchdata.js
@@ -0,0 +1,30 @@
+var indexSectionsWithContent =
+{
+  0: "bcdefghimprstuz",
+  1: "cds",
+  2: "c",
+  3: "bcdefghimprstu",
+  4: "s",
+  5: "rz"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "functions",
+  4: "groups",
+  5: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Namespaces",
+  3: "Functions",
+  4: "Modules",
+  5: "Pages"
+};
+
